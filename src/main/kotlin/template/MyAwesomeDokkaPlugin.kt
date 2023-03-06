8f2be95adcc635c655608edc6394eb1730b9ac21
IgnatBeresnev <ignat.beresnev@jetbrains.com>
2023-03-06 15:04:47 +0100

    Update Kotlin & Dokka to 1.8.10

--- src/main/kotlin/template/MyAwesomeDokkaPlugin.kt
+++ src/main/kotlin/template/MyAwesomeDokkaPlugin.kt
@@ -1,7 +1,11 @@
 package template
 
 import org.jetbrains.dokka.plugability.DokkaPlugin
+import org.jetbrains.dokka.plugability.DokkaPluginApiPreview
+import org.jetbrains.dokka.plugability.PluginApiPreviewAcknowledgement
 
 class MyAwesomeDokkaPlugin : DokkaPlugin() {
 
+    @OptIn(DokkaPluginApiPreview::class)
+    override fun pluginApiPreviewAcknowledgement() = PluginApiPreviewAcknowledgement
 }
