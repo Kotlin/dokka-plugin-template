28ad5bb1295e686b9869ee54064c7bd7c63c5851
IgnatBeresnev <ignat.beresnev@jetbrains.com>
2023-03-06 15:09:08 +0100

    Remove the opt-in

--- src/main/kotlin/template/MyAwesomeDokkaPlugin.kt
+++ src/main/kotlin/template/MyAwesomeDokkaPlugin.kt
@@ -1,11 +1,9 @@
 package template
 
 import org.jetbrains.dokka.plugability.DokkaPlugin
-import org.jetbrains.dokka.plugability.DokkaPluginApiPreview
 import org.jetbrains.dokka.plugability.PluginApiPreviewAcknowledgement
 
 class MyAwesomeDokkaPlugin : DokkaPlugin() {
 
-    @OptIn(DokkaPluginApiPreview::class)
     override fun pluginApiPreviewAcknowledgement() = PluginApiPreviewAcknowledgement
 }
