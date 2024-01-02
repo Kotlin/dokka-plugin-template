70c1e519206dbbd70fbec3b5a2f50e751b9547b9
Oleg Yukhnevich <whyoleg@gmail.com>
2024-01-02 15:02:54 +0200

    Actualize sample: * Kotlin/Dokka 1.9.10 * Gradle 8.5 * rename gradle project name to be the same as repository name * remove jcenter * use jvmToolchain * cleanup build logic

--- src/main/kotlin/template/MyAwesomeDokkaPlugin.kt
+++ src/main/kotlin/template/MyAwesomeDokkaPlugin.kt
@@ -2,8 +2,10 @@ package template
 
 import org.jetbrains.dokka.plugability.DokkaPlugin
 import org.jetbrains.dokka.plugability.PluginApiPreviewAcknowledgement
+import org.jetbrains.dokka.plugability.DokkaPluginApiPreview
 
 class MyAwesomeDokkaPlugin : DokkaPlugin() {
 
+    @DokkaPluginApiPreview
     override fun pluginApiPreviewAcknowledgement() = PluginApiPreviewAcknowledgement
 }
